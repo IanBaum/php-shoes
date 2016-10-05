39372b4c89bbad982428af6dde4b7e73ecd2b83b
Ian Baum <ian@ianbaum.com>
2016-10-05 10:40:45 -0400

    add the methods getId get Name SetName save, getAll deleteAll delete find and update to Brand class

--- src/Brand.php
+++ src/Brand.php
@@ -2,6 +2,77 @@
     class Brand
     {
         private $name;
-        private $
+        private $id;
+
+        function __construct($name, $id = null)
+        {
+            $this->name = $name;
+            $this->id = $id;
+        }
+
+        function getId()
+        {
+            return $this->id;
+        }
+
+        function getName()
+        {
+            return $this->name;
+        }
+
+        function setName($new_name)
+        {
+            $this->name = (string) $new_name;
+        }
+
+        function save()
+        {
+            $GLOBALS['DB']->exec("INSERT INTO brands (name) VALUES ('{$this->getName()}');");
+            $this->id = $GLOBALS['DB']->lastInsertId();
+        }
+
+        $static function getAll()
+        {
+            $returned_brands = $GLOBALS['DB']->query("SELEcT * FROM brands;");
+            $stores = [];
+            foreach($returned_brands as $brand){
+                $name = $brand['name'];
+                $id = $brand['id'];
+                $new_brand = new Brand($name, $id);
+                array_push($brands, $new_brand);
+            }
+            return $brands;
+        }
+
+        static function deleteAll()
+        {
+          $GLOBALS['DB']->exec("DELETE FROM brands;");
+        }
+
+        function delete()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM brands WHERE id = {$this->getId()};");
+        }
+
+        static function find($search_id)
+        {
+            $found_brand = null;
+            $brands = Brand::getAll();
+            foreach($brands as $brand)
+            {
+                $brand_id = $brand->getId();
+                if($brand_id = $searh_id)
+                {
+                    $found_brand = $brand;
+                }
+            }
+            return $found_brand;
+        }
+
+        function update($new_name)
+        {
+            $GLOBALS['DB']->exec("UPDATE brands SET name = '{new_name}' WHERE id = {$this->getId()};");
+            $this->setName($new_name;)
+        }
     }
 ?>
