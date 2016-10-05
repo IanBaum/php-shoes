271aba19f634ede835dbcde11717f4767b6b3db8
Ian Baum <ian@ianbaum.com>
2016-10-05 13:48:27 -0400

    start silex pages for stores and brands

--- app/app.php
+++ app/app.php
@@ -1,9 +1,11 @@
 <?php
     require_once __DIR__."/../vendor/autoload.php";
-    require_once __DIR__."src/Brand.php";
-    require_once __DIR__."src/Store.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
 
-    $server = 'mysql:host=localhost:8889;dbname=shoes'
+     date_default_timezone_set('America/New_York');
+
+    $server = 'mysql:host=localhost:8889;dbname=shoes';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
@@ -17,4 +19,34 @@
     $app->get("/", function() use($app){
         return $app['twig']->render('index.html.twig');
     });
+
+    //STORES
+    $app->get("/stores", function() use($app){
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->post("/stores", function() use($app){
+        $name = $_POST['store_name'];
+        $new_store = new Store($name);
+        $new_store->save();
+        return $app['twig']->render('stores.html.twig', array('stores' => Store::getAll()));
+    });
+
+    $app->get("/store/{store_id}", function($store_id) use($app){
+        return $app['twig']->render('store.html.twig', array('stores' => Store::getAll()));
+    });
+
+    //BRANDS
+    $app->get("/brands", function() use($app){
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    $app->post("/brands", function() use($app){
+        $name = $_POST['brand_name'];
+        $new_brand = new Brand($name);
+        $new_brand->save();
+        return $app['twig']->render('brands.html.twig', array('brands' => Brand::getAll()));
+    });
+
+    return $app;
 ?>
